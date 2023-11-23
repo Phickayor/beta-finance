5c2a9b54b6587e548eaab8717a5c7e9f222f0811
Olufikayomi Jetawo <jetawof@gmail.com>
2023-11-23 15:19:14 +0100

    follow up

--- pages/auth/authUtils.js
+++ pages/auth/authUtils.js
@@ -1,11 +1,14 @@
 // authUtils.js
 import { useRouter } from "next/router";
+import React from "react";
 
-export const RedirectToLogin = () => {
+function RedirectToLogin() {
   const router = useRouter();
   if (typeof window !== "undefined") {
     router.push({
       pathname: "/auth"
     });
   }
-};
+}
+
+export default RedirectToLogin;
