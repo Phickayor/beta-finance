74bf4306f85d675b1015c68e79b2b0cff8afa490
Olufikayomi Jetawo <jetawof@gmail.com>
2023-11-27 23:36:56 +0100

    Invoice

--- components/Admin/AllInvoices.jsx
+++ components/Admin/AllInvoices.jsx
@@ -1,54 +1,69 @@
 import baseurl from "@/config/host";
 import Router from "next/router";
 import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 function AllInvoices(props) {
   const [invoices, setInvoices] = useState([]);
-  const [invoicesNumber, setInvoicesNumber] = useState(4);
+  const [invoicesNumber, setInvoicesNumber] = useState(0);
 
-  function OpenClient(event) {
+  function OpenInvoice(event) {
     Router.push({
-      pathname: `/clients/${props.userId}/${event}`
+      pathname: `/admin/clients/${props.ClientId}/${event}`
     });
   }
+  useEffect(() => {
+    const fetchInvoices = async () => {
+      try {
+        const res = await fetch(`${baseurl}/client/${props.ClientId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${props.accesstoken}`
+          }
+        });
+        const data = await res.json();
+        res.ok
+          ? (console.log(data.data),
+            setInvoices(data.data),
+            setInvoicesNumber(data.data.length))
+          : console.log(data.message);
+      } catch (error) {
+        console.log(error);
+        toast.error(`Error: ${error.message}`);
+      }
+    };
+    fetchInvoices();
+  }, [props.accesstoken, props.userId, props.ClientId]);
   return (
     <div className="mx-auto lg:w-11/12 flex flex-col gap-4">
       <h1 className="text-xl font-poppins-semibold">
         INVOICES({invoicesNumber})
       </h1>
-
-      <div className="bg-white px-3 md:px-6 p-6 rounded-lg flex justify-between gap-2">
-        <div className="self-center">
-          <h1 className="md:text-lg font-poppins-semibold">Garri Purchase</h1>
+      {/* Commenting till i can access server */}
+      {invoices ? (
+        invoices.map((invoice, index) => (
+          <div
+            key={index}
+            className="bg-white px-3 md:px-6 p-6 rounded-lg flex justify-between gap-2"
+          >
+            <div className="self-center">
+              <h1 className="md:text-lg font-poppins-semibold">
+                {invoice.productName}
+              </h1>
+            </div>
+            <div className="self-center">
+              <h3 className="md:text-lg font-poppins-semibold">View Invoice</h3>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>
+          <h1 className="font-poppins-semibold text-center py-5 text-xl">
+            Your Customers would appear here
+          </h1>
         </div>
-        <div className="self-center">
-          <h3 className="md:text-lg font-poppins-semibold">View Invoice</h3>
-        </div>
-      </div>
-      <div className="bg-white px-3 md:px-6 p-6 rounded-lg flex justify-between gap-2">
-        <div className="self-center">
-          <h1 className="md:text-lg font-poppins-semibold">Stationeries</h1>
-        </div>
-        <div className="self-center">
-          <h3 className="md:text-lg font-poppins-semibold">Food Stuffs</h3>
-        </div>
-      </div>
-      <div className="bg-white px-3 md:px-6 p-6 rounded-lg flex justify-between gap-2">
-        <div className="self-center">
-          <h1 className="md:text-lg font-poppins-semibold">BTS purchase</h1>
-        </div>
-        <div className="self-center">
-          <h3 className="md:text-lg font-poppins-semibold">View Invoice</h3>
-        </div>
-      </div>
-      <div className="bg-white px-3 md:px-6 p-6 rounded-lg flex justify-between gap-2">
-        <div className="self-center">
-          <h1 className="md:text-lg font-poppins-semibold">Baby things</h1>
-        </div>
-        <div className="self-center">
-          <h3 className="md:text-lg font-poppins-semibold">View Invoice</h3>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
