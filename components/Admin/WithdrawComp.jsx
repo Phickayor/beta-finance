c71b00aa7d1b19a53fd59ba74db3767e57be0a77
Olufikayomi Jetawo <jetawof@gmail.com>
2023-11-28 11:18:13 +0100

    Adding key

--- components/Admin/WithdrawComp.jsx
+++ components/Admin/WithdrawComp.jsx
@@ -3,26 +3,25 @@ import { ErrorFunction } from "@/config/checkerror";
 import baseurl from "@/config/host";
 import Cookies from "js-cookie";
 import Link from "next/link";
-import  Router  from "next/router";
+import Router from "next/router";
 import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 // import "@/styles/global.css"
 function WithdrawComp() {
+  const [accountName, setAccountName] = useState("");
+  const [accountNumber, setAccountNumber] = useState("");
+  const [bankCode, setBankCode] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [allBankCode, setAllBankCode] = useState([]);
 
-  const [accountName, setAccountName] = useState("")
-  const [accountNumber, setAccountNumber] = useState("")
-  const [bankCode, setBankCode] = useState("")
-  const [amount, setAmount] = useState(0)
-  const [loading, setLoading] = useState(false)
-  const [allBankCode, setAllBankCode] = useState([])
+  const token = Cookies.get("token") ? JSON.parse(Cookies.get("token")) : "";
 
-  const token = Cookies.get("token")? JSON.parse(Cookies.get("token")) : ""
-
-  console.log(token.accesstoken)
+  console.log(token.accesstoken);
 
   const makeWithdrawl = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const res = await fetch(`${baseurl}/payment/withdrawal`, {
         method: "POST",
         headers: {
@@ -35,165 +34,173 @@ function WithdrawComp() {
           bankCode,
           amount: Number(amount)
         })
-      })
+      });
 
-      const resData = await res.json()
+      const resData = await res.json();
 
-      console.log(resData)
+      console.log(resData);
 
-      if(res.ok){
-        setLoading(false)
+      if (res.ok) {
+        setLoading(false);
         Swal.fire({
           title: "Transfer Completed",
           timer: "4000",
           showConfirmButton: false,
-          icon: 'success'
-        }).then(()=>{
+          icon: "success"
+        }).then(() => {
           Router.push({
-            pathname: "/admin",
-          })
-        })
-      }else{
+            pathname: "/admin"
+          });
+        });
+      } else {
         Swal.fire({
           title: resData?.message,
           timer: "4000",
           showConfirmButton: false,
-          icon: 'error'
-        })
+          icon: "error"
+        });
       }
 
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       setLoading(false);
       Swal.fire({
         text: ErrorFunction(error),
         timer: 3500,
         icon: "error",
-        showConfirmButton: false,
+        showConfirmButton: false
       });
     }
-  }
+  };
 
   const getBankCode = async () => {
     try {
       const getBankCode = await fetch(`${baseurl}/payment/bankcode`, {
         method: "GET"
-      })
+      });
 
-      const response = await getBankCode.json()
+      const response = await getBankCode.json();
 
-      console.log(response?.data)
+      console.log(response?.data);
 
-      setAllBankCode([])
+      setAllBankCode([]);
     } catch (error) {
-      console.log("error", error)
+      console.log("error", error);
     }
-  }
+  };
 
   // useEffect(()=>{
   //   getBankCode()
   // },[])
 
   return (
-    <div style={{
-      width: "100%",
-      display:"flex",
-      flexDirection:"column",
-      justifyContent:"center",
-      alignItems:"center"
-    }}>
-      <div
+    <div
       style={{
-        fontSize: "23px",
-        fontWeight: "bold",
-        marginBottom:"30px"
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
       }}
-      >Withdraw From Wallet</div>
+    >
       <div
-      style={{
-        width: "50%",
-        backgroundColor:"white",
-        height:"auto",
-        padding:"15px",
-        borderRadius:"5px"
-      }}
+        style={{
+          fontSize: "23px",
+          fontWeight: "bold",
+          marginBottom: "30px"
+        }}
+      >
+        Withdraw From Wallet
+      </div>
+      <div
+        style={{
+          width: "50%",
+          backgroundColor: "white",
+          height: "auto",
+          padding: "15px",
+          borderRadius: "5px"
+        }}
       >
         <div
-        style={{
-          display:"flex",
-          flexDirection:"column",
-          gap: "35px"
-        }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "35px"
+          }}
         >
-          <input placeholder="Account Number"
-          type="number"
-          style={{
-            padding:"10px",
-            border:"2px solid black",
-            borderRadius:"3px"
-          }}
-          value={accountNumber}
-          onChange={(e)=>{
-            setAccountNumber(e.target.value)
-          }}
+          <input
+            placeholder="Account Number"
+            type="number"
+            style={{
+              padding: "10px",
+              border: "2px solid black",
+              borderRadius: "3px"
+            }}
+            value={accountNumber}
+            onChange={(e) => {
+              setAccountNumber(e.target.value);
+            }}
           />
-          <input placeholder="Account Name"
-          style={{
-            padding:"10px",
-            border:"2px solid black",
-            borderRadius:"3px"
-          }}
-          value={accountName}
-          onChange={(e)=>{
-            setAccountName(e.target.value)
-          }}
+          <input
+            placeholder="Account Name"
+            style={{
+              padding: "10px",
+              border: "2px solid black",
+              borderRadius: "3px"
+            }}
+            value={accountName}
+            onChange={(e) => {
+              setAccountName(e.target.value);
+            }}
           />
           <select
-          style={{
-            padding:"10px",
-            border:"2px solid black",
-            borderRadius:"3px"
-          }}
-          value={bankCode}
-          onChange={(e)=>{
-            setBankCode(e.target.value)
-          }}
+            style={{
+              padding: "10px",
+              border: "2px solid black",
+              borderRadius: "3px"
+            }}
+            value={bankCode}
+            onChange={(e) => {
+              setBankCode(e.target.value);
+            }}
           >
-            {
-              bankCodeData?.map((props)=>(
-                <option value={props?.code}>{props?.name}</option>
-              ))
-            }
-            
+            {bankCodeData?.map((props, index) => (
+              <option key={index} value={props?.code}>
+                {props?.name}
+              </option>
+            ))}
           </select>
-          <input placeholder="Amount to Withdraw"
-          type="number"
-          style={{
-            padding:"10px",
-            border:"2px solid black",
-            borderRadius:"3px"
-          }}
-          value={amount}
-          onChange={(e)=>{
-            setAmount(e.target.value)
-          }}
+          <input
+            placeholder="Amount to Withdraw"
+            type="number"
+            style={{
+              padding: "10px",
+              border: "2px solid black",
+              borderRadius: "3px"
+            }}
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
           />
           <div
-          style={{
-            backgroundColor: "#37053C",
-            fontSize:"15px",
-            // padding:"9px",
-            color:"white",
-            borderRadius:"5px",
-            display:"flex",
-            justifyContent:"center",
-            alignItems:"center",
-            height:"50px",
-            marginBottom:"20px",
-            cursor:"pointer"
-          }}
-          onClick={makeWithdrawl}
-          >{loading? "Loading..." : "Withdraw"}</div>
+            style={{
+              backgroundColor: "#37053C",
+              fontSize: "15px",
+              // padding:"9px",
+              color: "white",
+              borderRadius: "5px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "50px",
+              marginBottom: "20px",
+              cursor: "pointer"
+            }}
+            onClick={makeWithdrawl}
+          >
+            {loading ? "Loading..." : "Withdraw"}
+          </div>
         </div>
       </div>
     </div>
